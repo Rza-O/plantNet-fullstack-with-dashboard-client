63856c7c42966e890a5b436cbfa387bd7959f888
appoclayptor <shahreza1414@gmail.com>
2025-01-07 00:05:47 +0600

    plant is adding successfully in the DB

--- src/pages/Dashboard/Seller/AddPlant.jsx
+++ src/pages/Dashboard/Seller/AddPlant.jsx
@@ -1,7 +1,63 @@
 import { Helmet } from 'react-helmet-async'
 import AddPlantForm from '../../../components/Form/AddPlantForm'
+import { imageUpload } from '../../../Api/utils';
+import useAuth from '../../../hooks/useAuth';
+import { useState } from 'react';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import toast from 'react-hot-toast';
 
 const AddPlant = () => {
+  const { user } = useAuth();
+  const [uploadButtonText, setUploadButtonText] = useState({ name: 'Upload button' });
+  const [loading, setLoading] = useState(false);
+  const axiosSecure = useAxiosSecure();
+  // handle form submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // before fetch loading
+    setLoading(true);
+    const form = e.target;
+    const name = form.name.value;
+    const category = form.category.value;
+    const description = form.description.value;
+    const price = parseFloat(form.price.value);
+    const quantity = parseInt(form.quantity.value);
+    const image = form.image.files[0];
+    const imageURL = await imageUpload(image);
+
+    // seller Info
+    const seller = {
+      name: user?.displayName,
+      email: user?.email,
+      image: user?.photoURL,
+    };
+
+    // create plant object 
+    const plantData = {
+      name,
+      category,
+      description,
+      price,
+      quantity,
+      image: imageURL,
+      seller,
+    }
+
+    // save plant in db
+    try {
+      // post req
+      await axiosSecure.post('/plants', plantData)
+      toast.success("Plant added successfully!")
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoading(false);
+    }
+
+
+  }
+
+
   return (
     <div>
       <Helmet>
@@ -9,7 +65,7 @@ const AddPlant = () => {
       </Helmet>
 
       {/* Form */}
-      <AddPlantForm />
+      <AddPlantForm loading={loading} handleSubmit={handleSubmit} uploadButtonText={uploadButtonText} setUploadButtonText={setUploadButtonText} />
     </div>
   )
 }
