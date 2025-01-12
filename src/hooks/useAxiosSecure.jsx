11e2479c167fb8039f136d13cc5ccd2e25de5b9c
appoclayptor <shahreza1414@gmail.com>
2025-01-13 05:39:31 +0600

    fix: adjust loading state handling in Login component and add console log in useAxiosSecure hook

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -23,6 +23,7 @@ const useAxiosSecure = () => {
           logOut()
           // navigate to login
           navigate('/login')
+          console.log('hello')
         }
         return Promise.reject(error)
       }
